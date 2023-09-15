c755412dfe6a0d2c73afc9ac1297c72868edca6c
Meredith Alley <malley@olin.edu>
2023-09-15 15:22:55 -0400

    ants moving around! + nonworking pheromone grid

--- base_sim.py
+++ base_sim.py
@@ -4,25 +4,27 @@ import pygame, sys, random
 from pygame.locals import *
 pygame.init()
 
-# Tweak
-tweak_delay = 2
-tweak_turning = 1
-
-# Sim Setup
-ants_grid_x_y = 16
-anthill = [8, 8]
- 
 # Screen Setup
-BACKGROUND = 'gray'
+BACKGROUND = '#C4A484'
 FPS = 60
 clock = pygame.time.Clock()
 WINDOW_WIDTH = 400
 WINDOW_HEIGHT = 300
+
+# Tweak
+tweak_delay = 2
+tweak_turning = .3
+
+# Sim Setup
+ants_grid_x_y = 16
+anthill = [WINDOW_WIDTH/2, WINDOW_HEIGHT/2]
+pheromone_grid = np.zeros(WINDOW_WIDTH, WINDOW_HEIGHT)
+ 
  
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Ants + Anthills')
 
-direction = {
+directions = {
   0: [0, 1], 
   1: [1, 1], 
   2: [1, 0], 
@@ -40,12 +42,23 @@ class Ant:
     self.velocity = np.random.randint(7)
 
   def move(self):
-    self.x += direction[self.velocity][0]
-    self.y += direction[self.velocity][1]
+    pheromone_grid[self.x, self.y] += 2
+    self.x += directions[self.velocity][0]
+    self.y += directions[self.velocity][1]
+    self.x = self.x%WINDOW_WIDTH
+    self.y = self.y%WINDOW_HEIGHT
+
+  def explore(self):
     self.velocity = round(self.velocity + np.random.normal(scale = tweak_turning))%8
+    self.move()
+
+  def phero_check(self):
+    for direction in directions:
+      check_x = self.x + direction[0]
+      check_y = self.y + direction[1]
 
   def draw(self):
-    pygame.draw.rect(WINDOW, 'red',pygame.Rect(self.x + 50,self.y + 50, 5, 5))
+    pygame.draw.rect(WINDOW, 'red',pygame.Rect(self.x, self.y, 5, 5))
 
 
 ants = []
@@ -57,8 +70,10 @@ def first_spawn():
 
 def update() :
   for ant in ants:
-    ant.move()
+    ant.explore()
     ant.draw()
+  for space in pheromone_grid:
+    space -= 1
 
 last_time = 0
 
